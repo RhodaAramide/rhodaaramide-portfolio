2b2567657cdff47241b876327655e3e4bf3b9c19
RhodaAramide <olabisiabolarin5@gmail.com>
2024-08-25 05:56:23 +0100

    Added netlify forms

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -32,7 +32,8 @@ const Contact = () => {
             <div className='flex flex-col xl:flex-row gap-[30px]'>
                 {/* form */}
                 <div className="xl:w-[54%] order-2 xl:order-none">
-                    <form className="flex flex-col gap-6 p-10 bg-white/10 rounded-xl">
+                    <form name="contact" method="POST" data-netlify="true"
+                    className="flex flex-col gap-6 p-10 bg-white/10 rounded-xl">
                         <h3 className="text-4xl font-bold text-accent ">Let's work together</h3>
                         <p className="text-white/60">
                         I am interested in great opportunities - especially
